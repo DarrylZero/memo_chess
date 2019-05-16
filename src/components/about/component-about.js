08596e37f33a071858b9acc6e3bd7863fe371b32
Vladimir.Bogodukhov <vladimir.bogodukhov@billing.ru>
2019-05-16 13:57:57 +0300

    intermediate

--- src/components/about/component-about.js
+++ src/components/about/component-about.js
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 import './component-about.css';
+import {getStatistics, isOver} from '../../datautils/stat-utils';
 
 export default class ApplicationAbout extends Component {
 
     render() {
+        const {game:{field}} = this.props.state;
         return (
             <div className="app-about">
                 <h2> About </h2>
-                Something that describes this wonderful project.
+                   Something that describes this wonderful project.
+
+                <button  width="40" height="40" onClick={() => {
+                    getStatistics(field);
+                    isOver(field);
+                }} >asdasdsdsdasdasdsdasda</button>
+
+
             </div>
         );
     }
--- src/datautils/stat-utils.js
+++ src/datautils/stat-utils.js
@@ -1,53 +1,67 @@
 import CellStatus from "../consts/cell-status"
+
 const {CELL_STATUS_REVEALED} = CellStatus;
 
 export function getStatistics(field) {
-    const map = new Map(); // key = color
-    for (let rowIndex = 0; rowIndex < field.length; rowIndex++) {
-        const row = field[rowIndex];
-        for (let colIndex = 0; colIndex < row.length; colIndex++) {
-            const cell = row[colIndex];
+  const map = new Map(); // key = color
+  for (let rowIndex = 0; rowIndex < field.length; rowIndex++) {
+    const row = field[rowIndex];
+    for (let colIndex = 0; colIndex < row.length; colIndex++) {
+      const cell = row[colIndex];
 
+      if (!map.get(cell.color)) {
+        const initialValue = {
+          total: 0,
+          open: 0
+        };
 
-            if (!map.get(cell.color)) {
-                const initialValue = {
-                    total: 0,
-                    open: 0
-                };
+        map.set(cell.color, initialValue)
+      }
+      const colorItem = map.get(cell.color);
+      colorItem.total = colorItem.total + 1;
 
-                map.set(cell.color, initialValue)
-            }
-            const colorItem = map.get(cell.color);
-            colorItem.total = colorItem.total + 1;
-
-
-            if (cell.status === CELL_STATUS_REVEALED) {
-                colorItem.open = colorItem.open + 1;
-            }
-        }
+      if (cell.status === CELL_STATUS_REVEALED) {
+        colorItem.open = colorItem.open + 1;
+      }
     }
+  }
 
-    const statistics = [];
-    for (const [key, value] of map) {
-        statistics.push({...value, color: key});
+  const statistics = [];
+  for (const [key, value] of map) {
+    statistics.push({...value, color: key});
+  }
+
+  const totals = statistics.reduce((prev, current) => {
+    return {
+      color: null,
+      total: prev.total + current.total,
+      open: prev.open + current.open,
     }
+  }, {
+    color: null,
+    total: 0,
+    open: 0
+  });
 
+  return {
+    statistics,
+    totals
+  };
+}
 
-    const totals = statistics.reduce((prev, current) => {
+export function isOver(field) {
+  let reduce = field[0].reduce((prev, cell) => {
         return {
-            color: null,
-            total: prev.total + current.total,
-            open: prev.open + current.open,
+          total: prev.total + 1,
+          open: prev.open + (cell.status === CELL_STATUS_REVEALED ? 1 : 0)
         }
-    }, {
-        color: null,
+      },
+      {
         total: 0,
         open: 0
-    });
+      });
 
+  reduce.toString();
 
-    return {
-        statistics,
-        totals
-    };
-};
+}
+
