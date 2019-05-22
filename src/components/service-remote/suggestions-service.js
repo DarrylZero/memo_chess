b17361c3e998808cf4c3ab85acc77e76ccad6a77
Vladimir.Bogodukhov <vladimir.bogodukhov@billing.ru>
2019-05-22 15:40:23 +0300

    added unirest pack

--- src/components/service-remote/suggestions-service.js
+++ src/components/service-remote/suggestions-service.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import './suggestions-service.css'
+import unirest from 'unirest'
+
 
 export default class SuggestionsService extends Component {
 
@@ -28,13 +30,16 @@ export default class SuggestionsService extends Component {
         console.log('handleSubmit ');
 
         const fullUri1 = `${this.serviceUrl}/suggestions/suggest/2128506`;
-        const fullUri = `http://127.0.0.1:53432/memorizador/suggestions/suggest/000121112`;
+        // const fullUri = `http://127.0.0.1:53432/memorizador/suggestions/suggest/000121112`;
+        const fullUri = `http://127.0.0.1:53432/memorizador/suggestions/health`;
 
-        fetch(fullUri)
-            // .then(response => {
-            //     return response.json()
-            // })
-            .then(data => {
+
+        fetch(fullUri, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+        }).then(data => {
                 console.log('data ');
 
                 // this.setState({data})
@@ -43,9 +48,28 @@ export default class SuggestionsService extends Component {
                 console.log('rejected ');
 
             }
+        );
 
-            );
 
+// https://rapidapi.com/matchilling/api/chuck-norris
+
+        /*
+                unirest.get("https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random")
+                    .header("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com")
+                    .header("X-RapidAPI-Key", "9d676e1c58mshbf912c7d9227473p10963cjsn771a4dce622c")
+                    .header("accept", "application/json")
+                    .end(function (result) {
+                        console.log(result.status, result.headers, result.body);
+                    });
+        */
+
+        unirest.get("http://127.0.0.1:53432/memorizador/suggestions/suggest/000121112")
+        // .header("X-RapidAPI-Host", "matchilling-chuck-norris-jokes-v1.p.rapidapi.com")
+        // .header("X-RapidAPI-Key", "9d676e1c58mshbf912c7d9227473p10963cjsn771a4dce622c")
+            .header("Content-Type", "application/json")
+            .end(function (result) {
+                console.log(result.status, result.headers, result.body);
+            });
 
     }
 
