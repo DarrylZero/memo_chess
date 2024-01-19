6eb64c0e39059bac1f56158eece1279e84268198
Vladimir Bogodukhov <v.bogodukhov@lipt-soft.ru>
2024-01-19 15:31:31 +0300

    Fixed docker script.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
-import com.github.dockerjava.api.model.AuthConfig
+import com.bmuschko.gradle.docker.tasks.image.*
 
 plugins {
     id("com.github.node-gradle.node") version "7.0.1"
-    id("io.github.godfather1103.docker-plugin") version "2.5"
+    id("com.bmuschko.docker-remote-api") version "9.4.0"
 }
 
 task<Delete>("clear") {
@@ -19,24 +19,10 @@ task<Exec>("build") {
     dependsOn("npmInstall")
 }
 
-docker {
-    dockerBuildDependsOn.add("build")
-    dockerDirectory.value(project.projectDir.absolutePath)
-//    val user = (project.findProperty("docker.username") ?: "") as String
-    val password = (project.findProperty("docker.password") ?: "") as String
-    val email = (project.findProperty("docker.email") ?: "") as String
-    val name = (project.findProperty("docker.demo.imageName") ?: "demo") as String
-//    if (user.isNotEmpty() && password.isNotEmpty()) {
-//        auth.value(AuthConfig(user, password, email))
-//    }
-    dockerBuildArgs.put("GitTag", "1.0")
-    imageName.value("$name-kotlin")
-    dockerImageTags.add("1.0")
-    pushImageTag.value(false)
-    pushImage.value(false)
-    // since 2.0
-    platform.value("linux/arm64/v8")
-}
-tasks["dockerBuild"].dependsOn("build")
 
-println("tasks[\"dockerBuild\"] => ${tasks["dockerBuild"].javaClass.name}")
+tasks.create("buildImage", DockerBuildImage::class) {
+    inputDir.set(file("build"))
+    dockerFile.set(file("Dockerfile"))
+}.apply {
+    group = "build"
+}
